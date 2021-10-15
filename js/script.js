23a69cd4dc49a94361f34bf4f22d1c4589576073
callmejogi <kutafin01@gmail.com>
2021-10-15 12:05:30 +0300

    upd

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const n = +prompt(`Type a number:`);
 
+if(n < 1){
+    alert(`NaN!`);
+};
+
 for(let i = 2; i <= n; i++){
     
     if(n % i === 0){
@@ -15,6 +19,4 @@ if(n === 1){
     alert(`Делитель само число: ${n}!`);
 };
 
-if(n < 1){
-    alert(`NaN!`);
-};
+
