494c6d1188882cfdd2b26b196e371bbe3f6f4843
callmejogi <kutafin01@gmail.com>
2021-10-16 21:33:37 +0300

    rightVersion

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 const n = +prompt(`Type a number:`);
 
 if(n < 1){
-    alert(`NaN!`);
+    alert(`it is NaN!`);
 };
 
-for(let i = 2; i <= n; i++){
+if(isNaN(n)){
+    alert(`NaN!`);
+}else{
+    for(let i = 2; i <= n; i++){
     
-    if(n % i === 0){
-        console.log(i);
-        break;
+        if(n % i === 0){
+            console.log(i);
+            break;
+        }else if(n === 1){
+            alert(`Делитель само число: ${n}!`);
+        };
     }
-
-    console.log(i)
-
 }
 
-if(n === 1){
-    alert(`Делитель само число: ${n}!`);
-};
+
+
+
 
 
